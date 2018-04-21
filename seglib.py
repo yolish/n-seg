dea67d330fc41b23f3f84c4ac64b9b8860b81d26
yolish <yolisha@gmail.com>
2018-04-21 15:42:00 +0100

    added segmentation classes and adapted to new scikit req.

--- seglib.py
+++ seglib.py
@@ -1,17 +1,18 @@
 import numpy as np
-from skimage.measure import label
+from skimage.measure import regionprops
 from scipy.ndimage.interpolation import map_coordinates
 from scipy.ndimage.filters import gaussian_filter
-#from skimage.segmentation import mark_boundaries, find_boundaries, label
+#from skimage.segmentation import mark_boundaries, find_boundaries
 from skimage.exposure import adjust_gamma, rescale_intensity, equalize_hist, equalize_adapthist
 from skimage.util import img_as_float
 from skimage.segmentation import quickshift, felzenszwalb, slic
 import random
 from skimage.color import rgb2lab, rgb2grey
-from skimage import filter
+from skimage import filters
 from scipy import ndimage as ndi
 import cv2
-from skimage.morphology import disk, opening, binary_opening
+from skimage.morphology import label, convex_hull_object
+from skimage.feature import canny
 
 
 class Segmentation(object):
@@ -24,22 +25,41 @@ class Segmentation(object):
 
 class ThreshSegmentation(Segmentation):
 
-    def __init__(self, name, threshold_func, separate_objects):
+    def __init__(self, name, threshold_func, block_size=None):
         super(ThreshSegmentation, self).__init__(name)
         self.threshold = threshold_func
-        self.separate_objects = separate_objects
+        self.block_size = block_size
 
     def __call__(self, img):
         grey_img = rgb2grey(img)
-        t = self.threshold(grey_img)
-        mask = label(grey_img > t)
-        if self.separate_objects:
-            pass
+        if self.block_size is not None:
+            t = self.threshold(grey_img, self.block_size)
+        else:
+            t = self.threshold(grey_img)
+        mask = (grey_img > t).astype(np.uint8)
+        if (np.sum(mask == 1) > np.sum(mask == 0)):
+            mask = np.invert(mask)
+
+        return label(mask)
+
+class EdgeSegmentation(Segmentation):
+
+    def __init__(self):
+        super(EdgeSegmentation, self).__init__("edge")
+
+
+    def __call__(self, img):
+        grey_img = rgb2grey(img)
+        mask = canny(grey_img)
+        mask = ndi.binary_fill_holes(mask)
+        #t = filters.threshold_minimum(mask)
+        #mask = mask > t
         return mask
 
 class MorphSegmentation(Segmentation):
-    def __init__(self, name, radius):
-        super(ThreshSegmentation, self).__init__(name)
+    def __init__(self, radius):
+        name = "morph-" + str(radius)
+        super(MorphSegmentation, self).__init__(name)
         self.radius = radius
 
     def __call__(self, img):
@@ -50,13 +70,24 @@ class MorphSegmentation(Segmentation):
         grey_img = cv2.threshold(grey_img, 0, 255, cv2.THRESH_OTSU)[1]
         if (np.sum(grey_img == 255) > np.sum(grey_img == 0)):
             grey_img = cv2.bitwise_not(grey_img)
-        mask = cv2.morphologyEx(grey_img, cv2.MORPH_OPEN, circle)
+        mask = cv2.morphologyEx(grey_img, cv2.MORPH_OPEN, ellipse)
         # label connected components
         return cv2.connectedComponents(mask)[1]
 
 
 
 
-#SEG_ALGS = {0:seg3}
-SEG_ALGS = {0:ThreshSegmentation("yen",filter.threshold_yen, False)}
-#SEG_ALGS = {0:seg1, 1:seg2, 2:seg3}
+'''
+SEG_ALGS = {0:EdgeSegmentation()}
+SEG_ALGS = {0:ThreshSegmentation("minimum",filters.threshold_minimum, False),
+            1:ThreshSegmentation("mean", filters.threshold_mean, False),
+            2: ThreshSegmentation("triangle", filters.threshold_triangle, False)}
+'''
+
+SEG_ALGS = {0:ThreshSegmentation("isodata",filters.threshold_isodata),
+            1:ThreshSegmentation("adaptive", filters.threshold_local, block_size=15),
+            2:ThreshSegmentation("yen", filters.threshold_yen),
+            3:MorphSegmentation(5),
+            4:MorphSegmentation(7),
+            5:ThreshSegmentation("minimum", filters.threshold_minimum)}
+
